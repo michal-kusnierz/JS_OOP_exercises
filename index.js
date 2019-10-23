515783a372cee6aac7faceac3a7956a68e4e04a8
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-23 19:25:18 +0200

    issue1 - to create two Objects with students data

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello')
+//day0 - we use ES5 today (so e.g. without const and let)
+
+//issue#1 - create two Objects containing data about students
+
+var Object1 = {
+    name: 'Kaj',
+    address: 'Poznan',
+    studentId: 1,
+    profile:'student',
+    displayFullInfo: function(){
+        console.log(this.name, this.address, this.studentId, this.profile);       //first try
+    },
+    changeId: function(){console.log(this.studentId=11);
+    }
+};
+
+var Object2 = {
+    name: 'Rafal',
+    address: 'Gdynia',
+    studentId: 2,
+    profile:'student',
+    displayFullInfo: function(){
+        console.log(this.name, this.address, this.studentId, this.profile);
+    },
+    changeId: function(){console.log(this.studentId=11);
+    }
+};
