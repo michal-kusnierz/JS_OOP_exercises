01616f71b22ba6ce3da967dbac79d76c160fe76d
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-23 19:54:42 +0200

    changed method to function constructor

--- index.js
+++ index.js
@@ -35,53 +35,45 @@ var Object2 = {
 
 var studentList = [];
 
-var Person ={
-    name: '',
-    // setName: function(name){this.name=name}, 
-    address: '',
-    studentId:'',
-    setData: function(name, address, studentId){
-        this.name = name,
-        this.address = address,
-        this.studentId = studentId
-    },
-    displayPerson: function(){
-        console.log(setData)
+// var Person ={
+//     name: '',
+//     // setName: function(name){this.name=name}, 
+//     address: '',
+//     studentId:'',
+//     setData: function(name, address, studentId){
+//         this.name = name,
+//         this.address = address,
+//         this.studentId = studentId
+//     },
+//     displayPerson: function(){
+//         console.log(setData)
+//     }
+// };
+
+// var student1 = Object.create(Person);
+// student1.setData('Adam', 'Gdynia', 1);
+// studentList.push(student1);
+
+function Person(name, address, studentId) {
+    this.name=name;
+    this.address=address;
+    this.studentId=studentId;
+    this.changeId=function(newId){
+        this.studentId=newId;
+        }   
     }
-};
-
-var student1 = Object.create(Person);
-student1.setData('Adam', 'Gdynia', 1);
-studentList.push(student1);
-
-var student2 = Object.create(Person);
-student2.setData('Monika', 'Gdansk', 2);
-studentList.push(student2);
-
-var student3 = Object.create(Person);
-student3.setData('Eustachy', 'Nowy Targ',3);
-var student4 = Object.create(Person);
-student4.setData('Dionizy','Olsztyn',4);
-var student5 = Object.create(Person);
-student5.setData('Pelagia', 'Gdansk', 5);
-var student6 = Object.create(Person);
-student6.setData('Innocenty', 'Gdansk', 6);
-var student7 = Object.create(Person);
-student7.setData('Maria', 'Gdansk', 7);
-var student8 = Object.create(Person);
-student8.setData('Anna', 'Gdansk', 8);
-var student9 = Object.create(Person);
-student9.setData('Jakub', 'Gdansk', 9);
-var student10 = Object.create(Person);
-student10.setData('Maciej', 'Gdansk', 10);
-studentList.push(student3);
-studentList.push(student4);
-studentList.push(student5);
-studentList.push(student6);
-studentList.push(student7);
-studentList.push(student8);
-studentList.push(student9);
-studentList.push(student10);
+    var studentList = [
+        new Person('Adam', 'Gdynia', 1),
+        new Person('Monika', 'Gdansk', 2),
+        new Person('Eustachy', 'Nowy Targ',3),
+        new Person('Dionizy','Olsztyn',4),
+        new Person('Pelagia', 'Gdansk', 5),
+        new Person('Innocenty', 'Gdansk', 6),
+        new Person('Maria', 'Gdansk', 7),
+        new Person('Anna', 'Gdansk', 8),
+        new Person('Jakub', 'Gdansk', 9),
+        new Person('Maciej', 'Gdansk', 10)
+    ]
 
 // implement into HTML
 
