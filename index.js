c9faf6d16a80a4564705e9da9f45acf072ef2399
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-25 09:52:53 +0200

    how to hide part of an accountNo - take1

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ class Account {
     set name(newName){
         this._name = newName;
     }
-
     set accountNo(newAccountNo){
         this._accountNo = newAccountNo;
     }
-
 }
+
+//jak ukryć część numeru accountNo?
+// 1. sposób
+// const x = 'string',
+// x[1] => x
+// for (2->22)
+// x[i]='*';
+
+// 2. sposób:
+// funkcją slice
+
+const x = '10111011101110111011101110'
+
 
 const Array =[
 new Account('Adam', 10222233334444555566667777),
