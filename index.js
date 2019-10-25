9e798b3f9cfe78b7c8e8c01542efac153758db29
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-25 10:26:38 +0200

    testing protect_account function

--- index.js
+++ index.js
@@ -33,6 +33,15 @@ class Account {
 
 const x = '10111011101110111011101110'
 
+protect_accountNo = function (x) {
+    var part1, hidden, part2;
+    part1 = x.slice(0,2)
+    hidden = x.slice(2,22);
+    part2 = x.slice(22,25)
+    return part1 + '**** **** **** **** ****' + part2;
+};
+
+console.log(protect_accountNo(x));
 
 const Array =[
 new Account('Adam', 10222233334444555566667777),
