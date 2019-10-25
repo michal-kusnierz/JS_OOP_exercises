aa0bd62c56845bfa418880a933561e0c7a4a0538
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-25 13:44:45 +0200

    first move done - class Move with static method Render and KeySupport

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 class Gamer{
     constructor(x,y){
-        this._x=x;
-        this._y=y;
+        this.x=x;
+        this.y=y;
+    }
+// take 1: one direction for start
+    left(){     
+        this.x -=1
     }
 }
+const ludzikJS = new Gamer(350,300);
+
+class Move {           
+    static RenderLudzik(ludzikJS){
+     const x = document.querySelector('.gamer');
+     console.log(ludzikJS, x);
+     x.style.left = ludzikJS.x +'px';
+     x.style.top = ludzikJS.y +'px';
+    }
+
+    static KeySupport(ludzikJS, event){
+     
+        if (event.key === 'ArrowLeft'){
+            ludzikJS.left();
+            Move.RenderLudzik(ludzikJS);
+        /*
+        1. switch
+        2. odświeżenie funkcji Render
+        */
+        }
+    }
+}
+
+Move.RenderLudzik(Gamer);
+
+document.addEventListener('keydown', (event) => Move.KeySupport(ludzikJS, event));
 
