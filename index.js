7a4e9135b07b1d3fc14bfc5ba7ecfe237a98f74c
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-25 09:31:45 +0200

    issue0

---
+++ index.js
@@ -0,0 +1,21 @@
+//day2
+
+class Account {
+    constructor(name, accountNo) {
+    this.name = name;
+    this._accountNo = accountNo;
+    }
+
+    get name (){
+        return this.name;
+    }
+
+    set name(){
+        this.name = newName;
+    }
+}
+
+const Array =[
+const item1 = new Account('Adam', 10222233334444555566667777),
+const item2 = new Account('Beata', 11222233334444555566667777)
+]
