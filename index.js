98b776b5b7e41fed1cb3c4c225791b1ca68ca1d1
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-25 09:37:28 +0200

    new class Account and test Array

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 class Account {
     constructor(name, accountNo) {
-    this.name = name;
+    this._name = name;
     this._accountNo = accountNo;
     }
 
     get name (){
-        return this.name;
+        return this._name;
     }
 
     set name(){
-        this.name = newName;
+        this._name = newName;
     }
 }
 
 const Array =[
-const item1 = new Account('Adam', 10222233334444555566667777),
-const item2 = new Account('Beata', 11222233334444555566667777)
+new Account('Adam', 10222233334444555566667777),
+new Account('Beata', 11222233334444555566667777)
 ]
