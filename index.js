47951e161139f13210741dfd694f04de97018cfc
michal-kusnierz <kusnierz.michal@gmail.com>
2019-10-27 23:40:29 +0100

    added static method for collision

--- index.js
+++ index.js
@@ -27,14 +27,12 @@ const BirdJS = new Gamer(350,300);
 
 class Move {           
     static RenderBird(BirdJS){
-     const x = document.querySelector('.gamer');
-     const y = document.querySelector('.gamer');
-     console.log(BirdJS, y);
-     
-     x.style.left = BirdJS.x +'px';
-     x.style.right = BirdJS.x +'px';
-     y.style.top = BirdJS.y +'px';
-     
+    const BirdDom = document.querySelector('.gamer');
+    //  const y = document.querySelector('.gamer');
+    //  console.log(BirdJS);
+     BirdDom.style.left = BirdJS.x +'px';
+     BirdDom.style.right = BirdJS.x +'px';
+     BirdDom.style.top = BirdJS.y +'px';
     }
 
     static KeySupport(BirdJS, event){
@@ -55,34 +53,27 @@ class Move {
           BirdJS.down();
           Move.RenderBird(BirdJS);
           break;
+        default:
+          Move.RenderBird(BirdJS);
       }
-     //changed to switch method
-        // if (event.key === 'ArrowLeft'){
-        //     BirdJS.left();
-        //     Move.RenderBird(BirdJS);
-        // }else if
-        // (event.key === 'ArrowRight'){
-        //     BirdJS.right();
-        //     Move.RenderBird(BirdJS);
-        // }else if
-        //     (event.key === 'ArrowDown'){
-        //     BirdJS.down();
-        //     Move.RenderBird(BirdJS);
-        // }else if
-        // (event.key === 'ArrowUp'){
-        // BirdJS.up();
-        // Move.RenderBird(BirdJS);
- 
-        // }
-        /*
-        1. switch
-        2. odświeżenie funkcji Render
-        */
+    }         
+     
+    static OutOfCage(BirdJS){
+      if (BirdJS.x === 0 || BirdJS.x === 680 || BirdJS.y === 0 || BirdJS.y === 580){   
+          document.querySelector('.board').style.background = 'skyblue';
+          document.querySelector('.board').style.border = 'none';
+          document.querySelector('.gamer').style.width = '50px';
+          setTimeout(eyeBlink, 150); 
+          function eyeBlink(){
+            alert('Bird has escaped from the cage');
+            location.reload();}
+        // }else{
+        //   Move.RenderBird(BirdJS);
         }
     }
-
+  }
 
 Move.RenderBird(BirdJS);
-
 document.addEventListener('keydown', (event) => Move.KeySupport(BirdJS, event));
+document.addEventListener('keydown', (event) => Move.OutOfCage(BirdJS, event));
 
